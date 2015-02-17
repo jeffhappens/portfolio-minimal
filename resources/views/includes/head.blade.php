d19d754b6780fbd233185f5111dc9daa11b1ce17
Jeff Mills <jeffrey.mills@outlook.com>
2015-02-17 13:36:52 -0600

    Initial Commit

---
+++ resources/views/includes/head.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]><html class="no-js lt-ie9 lt-ie8 lt-ie7"><![endif]-->
+<!--[if IE 7]><html class="no-js lt-ie9 lt-ie8"><![endif]-->
+<!--[if IE 8]><html class="no-js lt-ie9"><![endif]-->
+<!--[if gt IE 8]><!--><html class="no-js"><!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Jeffrey Mills | Full Stack Web Developer | New Orleans, LA</title>
+        <meta name="description" content="Jeffrey Mills provides cutting-edge web development services in the greater New Orleans Area">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+        <link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700' rel='stylesheet' type='text/css'>
+        <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700' rel='stylesheet' type='text/css'>
+        <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/app.css">
+        <script src="js/vendor/modernizr-2.6.2.min.js"></script>
+    </head>
---
+++ resources/views/site/index.blade.php
@@ -0,0 +1,48 @@
+@include('includes.head')
+    <body>
+        <!--[if lt IE 7]>
+            <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+
+        <!-- Add your site or application content here -->
+        <footer>
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-4">
+                        <h1>Jeffrey Mills</h1>
+                        <h3>Full Stack Web Developer</h3>
+                        <p>Phone: 443.944.6597</p>
+                        <p>Email: <a href="mailto:jeffrey.mills@outlook.com">jeffrey.mills@outlook.com</a></p>
+                        <p>Resume: <a href="">jeffrey.mills.resume.pdf</a></p>
+                    </div>
+                    <div class="col-md-8">
+                        <ul class="list-inline text-center">
+                            <li>
+                                <a href="https://github.com/jeffhappens">
+                                    <img src="/img/github-128-gray.png" />
+                                </a>
+                            </li>
+                            <li>
+                                <a href="https://www.linkedin.com/pub/jeffrey-mills/48/777/b87/en">
+                                    <img src="/img/linkedin-128-black.png" />
+                                </a>
+                            </li>
+                            <li>
+                                <a href="https://twitter.com/jeffreyamills">
+                                    <img src="/img/twitter-128-blue.png" />
+                                </a>
+                            </li>
+                            <li>
+                                <a href="https://www.facebook.com/jeffrey.mills.501598">
+                                    <img src="/img/facebook-128-blue.png" />
+                                </a>
+                            </li>
+                        </ul>
+                    </div>                    
+                </div>
+            </div>
+        </footer>
+
+        @include('includes.scripts')
+    </body>
+</html>
